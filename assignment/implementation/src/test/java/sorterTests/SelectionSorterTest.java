c14664a28aa2cda1514f8425b4655bd4c0ad3388
Niklas-Mezynski <n.mezynski@student.fontys.nl>
2021-11-17 15:25:02 +0100

    Few more tests

--- assignment/implementation/src/test/java/sorterTests/SelectionSorterTest.java
+++ assignment/implementation/src/test/java/sorterTests/SelectionSorterTest.java
@@ -19,15 +19,53 @@ public class SelectionSorterTest {
         //Creating and randomly filling the queue
         SortKind sortKind = SortKind.SELECTION;
         Queue<Integer> queue = factory.createPreferredQueue( sortKind );
-        List<Integer> sortedListForComparison = new ArrayList<>();
         fillRandom( queue, 100 );
-        queue.forEach(sortedListForComparison::add);
-        Collections.sort(sortedListForComparison);
+        //Creating the sorter
         Comparator<Integer> comp = Comparator.naturalOrder();
         Sorter<Integer> sorter = factory.createSorter( sortKind,  comp );
+        //Creating an ArrayList and sort it in order to compare later on
+        List<Integer> sortedListForComparison = new ArrayList<>();
+        queue.forEach(sortedListForComparison::add);
+        Collections.sort(sortedListForComparison);
+        //Sorting the queue and checking if its in right order
         Queue<Integer> sortedQueue = sorter.sort( queue );
         assertThat( sortedQueue ).isSameAs( queue );
         assertThat(sortedQueue).containsExactlyElementsOf(sortedListForComparison);
+    }
+
+    @Test
+    void t02EmptyQueueTest() {
+        //Creating the queue
+        SortKind sortKind = SortKind.SELECTION;
+        Queue<Integer> queue = factory.createPreferredQueue( sortKind );
+        fillRandom( queue, 0 );
+        //Creating the sorter
+        Comparator<Integer> comp = Comparator.naturalOrder();
+        Sorter<Integer> sorter = factory.createSorter( sortKind,  comp );
+        //Sorting the queue and checking if its in right order
+        assertThatCode(() -> sorter.sort( queue )).doesNotThrowAnyException();
+    }
+
+    @Test
+    void t03PreSortedQueue() {
+        //Creating the queue
+        SortKind sortKind = SortKind.SELECTION;
+        Queue<Integer> queue = factory.createPreferredQueue( sortKind );
+        queue.put(1);
+        queue.put(2);
+        queue.put(3);
+        queue.put(7);
+        queue.put(12);
+        //Creating the sorter
+        Comparator<Integer> comp = Comparator.naturalOrder();
+        Sorter<Integer> sorter = factory.createSorter( sortKind,  comp );
+        //Creating an ArrayList and sort it in order to compare later on
+        List<Integer> sortedListForComparison = new ArrayList<>();
+        queue.forEach(sortedListForComparison::add);
+        Collections.sort(sortedListForComparison);
+        //Sorting the queue and checking if its in right order
+        assertThatCode(() -> sorter.sort( queue )).doesNotThrowAnyException();
+        assertThat(queue).containsExactlyElementsOf(List.of(1,2,3,7,12));
     }
 
     private void fillRandom(Queue<Integer> queue, int i) {
